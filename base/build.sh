2e9883aa650af77d124a04ad2c94ff60b188dade
Ken Moini <ken@kenmoini.com>
2021-03-10 14:46:58 -0500

    switch to podman

--- base/build.sh
+++ base/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -f Containerfile --build-arg "BASEIMAGE=registry.access.redhat.com/ubi8/ubi" .
+podman build -f Containerfile --build-arg "BASEIMAGE=registry.access.redhat.com/ubi8/ubi" .
